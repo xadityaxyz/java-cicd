25f2727990e737bde1ad05a1b5d8fc90fbdc3924
Aditya Wadekar <99280159+xadityaxyz@users.noreply.github.com>
2025-01-29 15:14:10 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "aditya wadekar !!";
+		return "new changes!!";
 	}
 
 	public static void main(String[] args) {
