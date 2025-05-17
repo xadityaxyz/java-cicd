5c0dec82a22ad6748752ed3453fb060e132443d9
Aditya Wadekar <99280159+xadityaxyz@users.noreply.github.com>
2025-05-17 09:29:26 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "new changes!!";
+		return "Checkmarx Testing Project";
 	}
 
 	public static void main(String[] args) {
